249ea1ca0c81bddc7bf166e51171a801deeec9a4
erikaosterbur <erikaosterbur@gmail.com>
2022-03-11 10:33:55 -0600

    HQL demonstration 2

--- src/main/java/com/test/project/App.java
+++ src/main/java/com/test/project/App.java
@@ -1,13 +1,19 @@
 package com.test.project;
-
-/**
- * Hello world!
- *
- */
-public class App 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import com.test.hib.controller.FindUserHql;
+import com.test.hib.model.User;
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        FindUserHql u = new FindUserHql();
+        u.findUser();
+        u.findUserSelect();
+        u.getRecordbyId();
+        u.getmaxSalary();
+
     }
 }
